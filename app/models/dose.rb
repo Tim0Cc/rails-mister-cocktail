e0c420ad1fcc97a557b8d0911bfba12896751198
Tim Cecatka <tim.cecatka@gmail.com>
2020-11-12 12:57:22 +0100

    app/models/dose.rb  ->  validates_associated not needed when validates_uniqueness_of :cocktail, scope: :ingredient

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,7 +3,7 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
-  validates_associated :cocktail
-  validates_associated :ingredient
+  # validates_associated :cocktail
+  # validates_associated :ingredient
   validates_uniqueness_of :cocktail, scope: :ingredient
 end
